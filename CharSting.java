7871f1870673d0accec6ba22e01fef6a2895e416
yeseul-Kim <one3244@naver.com>
2019-07-01 16:02:26 +0900

    init

---
+++ CharSting.java
@@ -0,0 +1,13 @@
+package org.opentutorials.javatutorials.numberstring;
+
+public class CharSting {
+
+	public static void main(String[] args) {
+		String a, b;
+		a = "coding";
+		b = " everybody";
+		System.out.println(a+b);
+	}
+	
+
+}
---
+++ Number.java
@@ -0,0 +1,11 @@
+package org.opentutorials.javatutorials.numberstring;
+
+public class Number {
+
+	public static void main(String[] args) {
+		
+		System.out.println(6/2);	
+
+	}
+
+}
